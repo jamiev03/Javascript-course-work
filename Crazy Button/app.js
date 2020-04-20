559d5acf0fe7d33f3d1509ccbd9c2776a03778c3
Jamie <jamie03.v@gmail.com>
2020-04-19 20:28:23 -0400

    add mouseover and button styles. Using window.InnerHeight and width in order to know the dimensions of screen

--- Crazy Button/app.js
+++ Crazy Button/app.js
@@ -1,3 +1,9 @@
 const button = document.querySelector('button');
 
-button.addEventListener('mouseover', )
+button.addEventListener('mouseover', () => {
+    console.log('mouseover working');
+    const height = Math.floor(Math.random() * window.innerHeight);
+    const width = Math.floor(Math.random() * window.innerWidth);
+
+    button.style.
+});
