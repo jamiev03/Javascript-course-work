d1d80a9668e80dc01c4ad867443f48f0d1a823c6
Jamie <jamie03.v@gmail.com>
2020-04-22 20:48:23 -0400

    using the input listener and e.target.name in order to retrieve the value of input

--- Form Events/app.js
+++ Form Events/app.js
@@ -3,10 +3,39 @@ const creditCardInput = document.querySelector('#cc');
 const termsCheckbox = document.querySelector('#terms');
 const veggieSelect = document.querySelector('#veggie');
 
+const formData = {};
+for(let input of [creditCardInput, termsCheckbox, veggieSelect]) {
+    input.addEventListener('input', e => {
+        formData[e.target.name] = e.target.value;
+        console.log(e.target.name, e.target.value);
+    })
+}
 
-form.addEventListener('submit', (e) => {
-    console.log('cc', creditCardInput.value);
-    console.log('terms', termsCheckbox.checked);
-    console.log('veggieSelect', veggieSelect.value);
-    e.preventDefault();
-});
+
+
+// form.addEventListener('submit', (e) => {
+//     console.log('cc', creditCardInput.value);
+//     console.log('terms', termsCheckbox.checked);
+//     console.log('veggieSelect', veggieSelect.value);
+//     e.preventDefault();
+// });
+
+
+
+
+
+// const formData = {};
+// creditCardInput.addEventListener('input', (e) => {
+//     console.log('cc changed', e);
+//     formData['cc'] = e.target.value;
+// });
+
+// veggieSelect.addEventListener('input', (e) => {
+//     console.log('veggieChanged', e);
+//     formData['veggie'] = e.target.value;
+// });
+
+// termsCheckbox.addEventListener('input', (e) => {
+//     console.log('Checkbox!', e);
+//     formData['agreeToTerms'] = e.target.checked;
+// });
