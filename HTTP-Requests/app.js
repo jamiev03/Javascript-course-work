64874a23f92870c0ef73897eb6fc2530a61cb8ef
Jamie <jamie03.v@gmail.com>
2020-05-08 21:21:04 -0400

    chaining axios requests

--- HTTP-Requests/app.js
+++ HTTP-Requests/app.js
@@ -1,4 +1,17 @@
 axios.get('https://swapi.dev/api/planets')
 .then(({ data }) => {
     console.log(data);
+    for(let planet of data.results) {
+        console.log(planet.name, planet.diameter);
+    }
+    return axios.get(data.next)
+})
+.then(({ data }) => {
+    console.log(data);
+    for(let planet of data.results) {
+        console.log(planet.name);
+    }
+})
+.catch(err => {
+    console.log('ERROR!!');
 });
