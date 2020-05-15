129de452000e7601daa4263f3e2414a0f55da6b9
Jamie <jamie03.v@gmail.com>
2020-05-14 21:14:31 -0400

    chaining together axios requests

--- HTTP-Requests/app.js
+++ HTTP-Requests/app.js
@@ -1,17 +1,19 @@
-axios.get('https://swapi.dev/api/planets')
-.then(({ data }) => {
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets') => {
+    return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
     console.log(data);
     for(let planet of data.results) {
         console.log(planet.name, planet.diameter);
     }
-    return axios.get(data.next)
-})
-.then(({ data }) => {
-    console.log(data);
-    for(let planet of data.results) {
-        console.log(planet.name);
-    }
-})
+    return fetchNextPlanets(data.next)
+};
+
+fetchNextPlanets()
+.then(printPlanets)
+.then(fetchNextPlanets)
+.then(printPlanets)
 .catch(err => {
     console.log('ERROR!!');
 });
